f18d58a799d1e906e841510f17363490d5941fb6
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-10-19 20:04:04 +0545

    CustomUser model created

--- getdocs/accounts/models.py
+++ getdocs/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    # email, username, first_name, last_name, etc. are included by default
+    is_seller = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile', null=True, blank=True)
