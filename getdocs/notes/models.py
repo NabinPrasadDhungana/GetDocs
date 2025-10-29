f97f1bed1bb500bd667f060c3376f72cb7107959
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-10-29 16:20:54 +0545

    Notes model added

--- getdocs/notes/models.py
+++ getdocs/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Category(models.Model):
@@ -7,3 +8,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Note(models.Model):
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='uploaded_notes')
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='notes')
+    file = models.FileField(upload_to='notes/')
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    
